8d348d738a509dc060fb55df72a98358c17896ab
Ross Keenan <ross.keenan@protonmail.com>
2022-02-16 16:12:48 -0600

    useCallback example. Good for expensive function calls.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import UseRefExample1 from "./components/UseRefExample1";
 import UseRefExample2 from "./components/UseRefExample2";
 import UseRefExample3 from "./components/UseRefExample3";
 import UseMemoExample from "./components/UseMemoExample";
+import UseCallbackExample from "./components/UseCallbackExample";
 
 function App() {
   return (
     <div className="container mt-5">
-      <UseMemoExample />
+      <UseCallbackExample />
     </div>
   );
 }
